1662b0b8ad4cb7371442286307a53cf7d42518dc
alevitt <alejandrovitt@hotmail.com>
2024-09-05 12:43:34 -0300

    inicio repositorio. Creando clase Moto

---
+++ Guia1/concecionaria/Moto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace concecionaria
+{
+    internal class Moto
+    {
+        public string Marca {  get; private set; }
+        public int Modelo { get; private set; }
+        public double ValorFrabricacion {  get; private set; }
+
+        //COnstructor
+        public Moto(string marca, int modelo , double valorFabricacion)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            ValorFrabricacion = valorFabricacion;
+        }
+    }
+}
