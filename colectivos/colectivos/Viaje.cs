7eb8d5e41df5fc84ba35c9b20b6b97d5ebf50a6a
alevittHome <alejandrovitt@hotmail.com>
2024-09-12 22:16:30 -0300

    Inicio clases, y terminado de uml

---
+++ colectivos/colectivos/Viaje.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace colectivos
+{
+    internal class Viaje
+    {
+        //VARIABLES DE ISTANCIAS
+        int hInicio, mInicio,transportado;
+
+        //PROPIEDADES
+        public int Transportados {
+            get { return transportado; }
+            private set
+            {
+                if(value < Asientos)
+                {
+
+                }
+            }
+        }
+        public int Asientos { get; private set; }
+        public int HDemora { get; private set; }
+        public int MDemora { get; private set; }
+        public int CantParadas { get; private set; }
+        public int HDuracionViaje { get; private set; }
+        public int MDuracionViaje { get; private set; }
+
+        //CONSTRUCTOR
+        public Viaje(int hInicio, int mInicio, int asientos)
+        {
+            this.hInicio = hInicio;
+            this.mInicio = mInicio;
+            Asientos = asientos;
+            Transportados = 0;
+        }
+
+        //METODOS
+        public void RealizarParada(int hLLegadaParada, int  mLLegadaParada, int hSalidaParada , int mSalidaParada, int ascienden, int descienden)
+        {
+            HDemora = hSalidaParada - hLLegadaParada;
+            MDemora = mSalidaParada - mLLegadaParada;
+            Transportados += ascienden;
+            Transportados -= descienden;
+
+        }
+
+
+    }
+}
